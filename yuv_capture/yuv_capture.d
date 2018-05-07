3ad928e219d11b31c5467ad74f7d4e79fad61f02
majg <majg@fullhan.com>
2018-05-07 18:03:39 +0800

    add fh8632 yuv capture demo

---
+++ yuv_capture/yuv_capture.d
@@ -0,0 +1,40 @@
+yuv_capture.d yuv_capture.o: yuv_capture.c \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/types/type_def.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_system_mpi.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_common.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_vpu_mpi.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_vpu_mpipara.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/sample_common_isp.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/libvlcview.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/dbi/dbi_over_tcp.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/sample_opts.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_venc_mpi.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_venc_mpipara.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_bgm_mpi.h \
+  /home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_bgm_mpipara.h
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/types/type_def.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_system_mpi.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_common.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_vpu_mpi.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_vpu_mpipara.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/sample_common_isp.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/libvlcview.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/dbi/dbi_over_tcp.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/common/sample_opts.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_venc_mpi.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_venc_mpipara.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_bgm_mpi.h:
+
+/home/duobao/work/FH8632_IPC_V1.0_20180330/media_support/demo/../include/dsp/fh_bgm_mpipara.h:
