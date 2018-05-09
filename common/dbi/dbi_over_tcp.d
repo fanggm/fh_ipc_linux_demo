8498601f60d2c4cb0b0982f0da0b943a47e8f839
majg <majg@fullhan.com>
2018-05-09 18:08:04 +0800

    rm file

--- common/dbi/dbi_over_tcp.d
+++
@@ -1,7 +0,0 @@
-../common/dbi/dbi_over_tcp.d ../common/dbi/dbi_over_tcp.o:  \
- ../common/dbi/dbi_over_tcp.c ../common/dbi/dbi_over_tcp.h \
-  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/demo/../include/di/debug_interface.h
-
-../common/dbi/dbi_over_tcp.h:
-
-/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/demo/../include/di/debug_interface.h:
