7d0da7c04ae9cceddd3db7498ba1999b5762162b
majg <majg@fullhan.com>
2018-05-09 18:06:35 +0800

    rm file

---
+++ fh8830_yuv_capture/sample_vlcview.d
@@ -0,0 +1,49 @@
+sample_vlcview.d sample_vlcview.o: sample_vlcview.c \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/types/type_def.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_system_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_common.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_jpeg_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_jpeg_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_common_isp.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/libvlcview.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/dbi/dbi_over_tcp.h \
+  config_1080p25.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/isp/isp_enum.h
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/types/type_def.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_system_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_common.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_jpeg_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_jpeg_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_common_isp.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/libvlcview.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/dbi/dbi_over_tcp.h:
+
+config_1080p25.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/isp/isp_enum.h:
---
+++ yuv_capture/yuv_capture.d
@@ -0,0 +1,40 @@
+yuv_capture.d yuv_capture.o: yuv_capture.c \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/types/type_def.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_system_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_common.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_common_isp.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/libvlcview.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/dbi/dbi_over_tcp.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_opts.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpipara.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpi.h \
+  /home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpipara.h
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/types/type_def.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_system_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_common.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_vpu_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_common_isp.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/libvlcview.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/dbi/dbi_over_tcp.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/common/sample_opts.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_venc_mpipara.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpi.h:
+
+/home/duobao/work/github_FH8x30_IPC_V1.3_20170823/media_support/fh_ipc_linux_demo/../include/dsp/fh_bgm_mpipara.h:
